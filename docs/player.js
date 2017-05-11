59f4aae30a0336814185abe8b1489f7f008e31fa
takumiokuhira <taku326.vaulter@gmail.com>
2017-05-11 12:27:45 +0900

    actionAgainstOmoteUra2を関数化

--- docs/player.js
+++ docs/player.js
@@ -1,16 +1,23 @@
+
 const GU = 0;
 const CHOKI = 1;
 const PA = 2;
 
+
 let matches = 0;
 
-function action(oppornent){
+function actionAgainstOmoteUra2(){
    matches = matches + 1;
    const amari = matches % 3;
    const isEven = amari == 0;
    console.log(`${matches} % 3 = ${amari} : ${isEven}`);
    if(isEven){
-      return CHOKI;
+      return result;
    }
-   return GU
+   return result;
 }
+ 
+ function action(oppornent){
+     let result = actionAgainstOmoteUra2()
+     return result;
+ }
